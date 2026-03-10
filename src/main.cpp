df3c5921225e01ef2b435a6b3aaab47cd7182062
Tomas Bivainis <bivainis.tomas@gmail.com>
2026-03-10 10:43:58 +0100

    added image class as an abstraction to cv::Mat

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 #include <opencv2/opencv.hpp>
+
+class Helper {
+public:
+    static void printPoint(std::vector<int> point) {
+        for(int i = 0; i < point.size(); i++) {
+            std::cout << point[i] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+};
+
+class Image {
+private:
+    int width;
+    int height;
+    cv::Mat image;
+public:
+    Image(std::string filename) {
+       image = cv::imread(filename, cv::IMREAD_COLOR_BGR); 
+
+        if(image.empty()) throw new std::runtime_error("No image found");
+
+        width = image.size().width;
+        height = image.size().height;
+    }
+
+    Image(cv::Mat image) {
+        this -> image = image;
+        width = image.size().width;
+        height = image.size().height;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    cv::Mat getImage() {
+        return image;
+    }
+
+    std::vector<int> getPoint(int row, int column) {
+        cv::Vec3b point = image.at<cv::Vec3b>(row, column);
+        std::vector<int> castedPoint(3);
+        
+        for(size_t i = 0; i < 3; i++) {
+            castedPoint[i] = static_cast<int>(point[i]);
+        }
+
+        return castedPoint; 
+    }
+};
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
@@ -12,16 +69,11 @@ int main(int argc, char *argv[]) {
     
     char *filename = argv[1];
 
-    cv::Mat image = cv::imread(filename, cv::IMREAD_COLOR_RGB);
+    Image *image = new Image(filename);
 
-    if(image.empty()) {
-        std::cout << "Image not found" << std::endl;
+    Helper::printPoint((image -> getPoint(0, 0)));
 
-        std::cin.get();
-        return -1;
-    }
-
-    cv::imshow("image", image);
+    cv::imshow("image", image -> getImage());
     cv::waitKey(0);
 
     return 0;
