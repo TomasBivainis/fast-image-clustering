234023a78463f6dc2d10b95bb82133c0980daea3
Tomas Bivainis <bivainis.tomas@gmail.com>
2026-03-22 18:29:03 +0100

    added simple arguments for k and epochs

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,21 @@
 #include "KMeans.hpp"
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) {
+    if(argc < 4) {
         std::cout << "Not enough arguments" << std::endl;
 
-        std::cin.get();
         return -1;
     } 
     
     char *filename = argv[1];
+    int k = std::stoi(argv[2]);
+    int epochs = std::stoi(argv[3]);
 
     cv::Mat opencv_image = cv::imread(filename);
 
     Image *image = new Image(opencv_image);
 
-    KMeans::cluster_image(image, 8, 3);
+    KMeans::cluster_image(image, k, epochs);
 
     cv::imshow("image", *image -> convertToMat());
     cv::waitKey(0);
